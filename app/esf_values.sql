e861f0d3223063415b4682e571f6948cb0af6dd7
zhenghang gu <guzhenghang@gmail.com>
2016-11-22 22:31:24 -0800

    try

---
+++ app/esf_values.sql
@@ -0,0 +1,18 @@
+
+/* SETTING UP ESF */
+
+INSERT INTO ESF VALUES (1,"Transportation");
+INSERT INTO ESF VALUES (2,"Communications");
+INSERT INTO ESF VALUES (3,"Public Works and Engineering");
+INSERT INTO ESF VALUES (4,"Firefighting");
+INSERT INTO ESF VALUES (5,"Emergency Management");
+INSERT INTO ESF VALUES (6,"Mass Care, Emergency Assistance, Housing, and Human Services");
+INSERT INTO ESF VALUES (7,"Logistics Management and Resource Support");
+INSERT INTO ESF VALUES (8,"Public Health and Medical Services");
+INSERT INTO ESF VALUES (9,"Search and Rescue");
+INSERT INTO ESF VALUES (10,"Oil and Hazardous Materials Response");
+INSERT INTO ESF VALUES (11,"Agriculture and Natural Resources");
+INSERT INTO ESF VALUES (12,"Energy");
+INSERT INTO ESF VALUES (13,"Public Safety and Security");
+INSERT INTO ESF VALUES (14,"Long-Term Community Recovery");
+INSERT INTO ESF VALUES (15,"External Affairs");
